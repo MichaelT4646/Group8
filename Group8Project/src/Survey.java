262bfefeca448bfe93c6bfc7b745df044e7119ad
MichaelT4646 <mtambascio@quinnipiac.edu>
2021-03-15 15:29:35 -0400

    Added stats to the survey as well as fixed some spelling and grammer errors

--- Group8Project/src/Survey.java
+++ Group8Project/src/Survey.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class Survey {
 	public static void main(String[] args) {
+		int i, i2, i3, j;
+
 		Scanner keyboardInput = new Scanner(System.in);
 		System.out.println(
 				"Hello, thank you for participating in this survey! \nPlease answer your questions on a scale from 0 being the worst and 10 being the best. \nPress any key to start the survey.");
 		String question0Answer = keyboardInput.nextLine();
 		int questionAnswers[] = new int[19];
 
-		System.out.println("Question 1: How would you rate your experience on Quinnipiac is year?");
+		System.out.println("Question 1: How would you rate the campus life at Quinnipiac this year?");
 		int question1Answer = keyboardInput.nextInt();
 		while (question1Answer > 10 || question1Answer < 0) {
 			System.out.println("Please input a number between 0-10.");
@@ -16,7 +18,7 @@ public class Survey {
 		}
 		questionAnswers[0] = question1Answer;
 
-		System.out.println("Question 2: How would you rate your online class experience at Quinnipiac?");
+		System.out.println("Question 2: How would you rate the atmosphere of your school before Covid?");
 		int question2Answer = keyboardInput.nextInt();
 		while (question2Answer > 10 || question2Answer < 0) {
 			System.out.println("Please input a number between 0-10.");
@@ -24,7 +26,7 @@ public class Survey {
 		}
 		questionAnswers[1] = question2Answer;
 
-		System.out.println("Question 3: How would you rate your in class experience at Quinnipiac?");
+		System.out.println("Question 3: How would you rate your online class experience at Quinnipiac?");
 		int question3Answer = keyboardInput.nextInt();
 		while (question3Answer > 10 || question3Answer < 0) {
 			System.out.println("Please input a number between 0-10.");
@@ -32,7 +34,7 @@ public class Survey {
 		}
 		questionAnswers[2] = question3Answer;
 
-		System.out.println("Question 4: How would you rate your in class experience before Covid?");
+		System.out.println("Question 4: How would you rate your in class experience at Quinnipiac?");
 		int question4Answer = keyboardInput.nextInt();
 		while (question4Answer > 10 || question4Answer < 0) {
 			System.out.println("Please input a number between 0-10.");
@@ -40,7 +42,7 @@ public class Survey {
 		}
 		questionAnswers[3] = question4Answer;
 
-		System.out.println("Question 5: How would you rate the food at Quinnipiac?");
+		System.out.println("Question 5: How would you rate your in class experience before Covid?");
 		int question5Answer = keyboardInput.nextInt();
 		while (question5Answer > 10 || question5Answer < 0) {
 			System.out.println("Please input a number between 0-10.");
@@ -48,7 +50,7 @@ public class Survey {
 		}
 		questionAnswers[4] = question5Answer;
 
-		System.out.println("Question 6: How would you rate the food from your school before covid?");
+		System.out.println("Question 6: How would you rate the food at Quinnipiac?");
 		int question6Answer = keyboardInput.nextInt();
 		while (question6Answer > 10 || question6Answer < 0) {
 			System.out.println("Please input a number between 0-10.");
@@ -56,7 +58,7 @@ public class Survey {
 		}
 		questionAnswers[5] = question6Answer;
 
-		System.out.println("Question 7: How would you rate the clubs that you are in?");
+		System.out.println("Question 7: How would you rate the food from your school before Covid?");
 		int question7Answer = keyboardInput.nextInt();
 		while (question7Answer > 10 || question7Answer < 0) {
 			System.out.println("Please input a number between 0-10.");
@@ -64,7 +66,7 @@ public class Survey {
 		}
 		questionAnswers[6] = question7Answer;
 
-		System.out.println("Question 8: How would you rate the clubs that you were in before Covid?");
+		System.out.println("Question 8: How would you rate the clubs that you are in?");
 		int question8Answer = keyboardInput.nextInt();
 		while (question8Answer > 10 || question8Answer < 0) {
 			System.out.println("Please input a number between 0-10.");
@@ -72,7 +74,7 @@ public class Survey {
 		}
 		questionAnswers[7] = question8Answer;
 
-		System.out.println("Question 9: How would you rate the activites offered by Quinnipiac?");
+		System.out.println("Question 9: How would you rate the clubs that you were in before Covid?");
 		int question9Answer = keyboardInput.nextInt();
 		while (question9Answer > 10 || question9Answer < 0) {
 			System.out.println("Please input a number between 0-10.");
@@ -80,13 +82,21 @@ public class Survey {
 		}
 		questionAnswers[8] = question9Answer;
 
-		System.out.println("Question 10: How would you rate the activites offered by your school before Covid?");
+		System.out.println("Question 10: How would you rate the activites offered by Quinnipiac?");
 		int question10Answer = keyboardInput.nextInt();
 		while (question10Answer > 10 || question10Answer < 0) {
 			System.out.println("Please input a number between 0-10.");
 			question10Answer = keyboardInput.nextInt();
 		}
 		questionAnswers[9] = question10Answer;
+
+		System.out.println("Question 11: How would you rate the activites offered by your school before Covid?");
+		int question11Answer = keyboardInput.nextInt();
+		while (question11Answer > 10 || question11Answer < 0) {
+			System.out.println("Please input a number between 0-10.");
+			question11Answer = keyboardInput.nextInt();
+		}
+		questionAnswers[10] = question11Answer;
 
 		System.out.println("Thank you for taking the time to complete this survey! It means a lot to us!");
 		System.out.println("These are your answers!");
@@ -95,5 +105,70 @@ public class Survey {
 			System.out.println(questionAnswers[counter]);
 			counter += 1;
 		}
+
+		System.out.println("Here are some statistics about your answers!");
+		System.out.println(" ");
+
+		int AverageBeforeCovid = (questionAnswers[1] + questionAnswers[4] + questionAnswers[6] + questionAnswers[8]
+				+ questionAnswers[10]) / 5;
+		int AverageAfterCovid = (questionAnswers[0] + questionAnswers[2] + questionAnswers[3] + questionAnswers[5]
+				+ questionAnswers[7] + questionAnswers[9]) / 6;
+
+		System.out.println("Your average score for questions before Covid was " + AverageBeforeCovid + ".");
+		System.out.println("Your average score for questions after Covid was " + AverageAfterCovid + ".");
+
+		int highestScore = questionAnswers[0];
+		int highestQuestionNumber = 0;
+		for (i = 0; i < questionAnswers.length; i++) {
+			if (highestScore < questionAnswers[i]) {
+				highestScore = questionAnswers[i];
+				highestQuestionNumber = i;
+			}
+		}
+		System.out.println("The highest score you gave was " + highestScore + " and you gave it for question "
+				+ highestQuestionNumber + ".");
+
+		int lowestScore = questionAnswers[0];
+		int lowestQuestionNumber = 0;
+		for (i2 = 0; i2 < questionAnswers.length; i2++) {
+			if (lowestScore > questionAnswers[i2]) {
+				lowestScore = questionAnswers[i2];
+				lowestQuestionNumber = i2;
+			}
+		}
+		System.out.println("The lowest score you gave was " + lowestScore + " and you gave it for question "
+				+ lowestQuestionNumber + ".");
+
+		int range = highestScore - lowestScore;
+
+		System.out.println("The range of your scores was " + range + ".");
+
+		int median = 0;
+		int n = questionAnswers.length;
+		if (n % 2 == 1) {
+			median = questionAnswers[(n + 1) / 2 - 1];
+		} else {
+			median = (questionAnswers[n / 2 - 1] + questionAnswers[n / 2]) / 2;
+		}
+		System.out.println("The median of your scores is " + median + ".");
+
+		int mode = 0;
+		for (i3 = 0; i3 < questionAnswers.length; i3++) {
+			int count = 0;
+			int maxOccured = 0;
+			for (j = 0; j < questionAnswers.length; j++) {
+				if (questionAnswers[i3] == questionAnswers[j]) {
+					count++;
+				}
+
+				if (maxOccured < count) {
+					maxOccured = count;
+					mode = questionAnswers[i3];
+				}
+			}
+		}
+		System.out.println("The score you gave the most was " + mode + ".");
+		System.out.println(" ");
+		System.out.println("Thank you again for taking our survey! We hope to see you again! :)");
 	}
 }
